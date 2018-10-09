7fbdefc7faf87275f03426f6b5ad495a1a077cdb
zk <2498695596@qq.com>
2018-10-09 22:04:17 +0800

    第一天

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+import {HashRouter,Route} from 'react-router-dom';
+class Router extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+      return(
+        <HashRouter>
+        <div>    
+        </div>
+    </HashRouter>
+      ) 
+    }
+}
+export default Router
---
+++ src/views/notMatch/index.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+ class NotMatch extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                <h1>404,没有你要找的页面。</h1>
+            </div>
+        )
+    }
+}
+export default NotMatch
